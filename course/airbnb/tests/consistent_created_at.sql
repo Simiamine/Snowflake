bffeb27b8d2445083c04c4b0c7c120f7cd09092f
Amine M'ZALI <mzaliamine@gmail.com>
2025-10-13 17:01:07 +0200

    Add data tests for dim_listings_cleansed model; implement minimum_row_count and positive_values tests

--- course/airbnb/tests/consistent_created_at.sql
+++ course/airbnb/tests/consistent_created_at.sql
@@ -1,11 +1,4 @@
-SELECT
-  r.review_id,
-  r.listing_id,
-  r.review_date,
-  l.created_at
-FROM {{ ref('fct_reviews') }} r
-JOIN {{ ref('dim_listings_cleansed') }} l
-  ON r.listing_id = l.listing_id
-WHERE r.review_date < l.created_at
-  AND r.review_date IS NOT NULL
-  AND l.created_at IS NOT NULL;
+SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }} r
+USING (listing_id)
+WHERE l.created_at >= r.review_date
